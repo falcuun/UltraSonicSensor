66346f1734da4d70266294c7fd347d530162ab02
falcuun <falcuun@gmail.com>
2019-01-09 23:53:53 +0100

    Added some comments

--- USSensor.ino
+++ USSensor.ino
@@ -1,6 +1,6 @@
 
-const int ECHO_PIN = 5;
-const int TRIGGER_PIN = 3;
+const int ECHO_PIN = 5; // Echo Pin on Sensor that sends data back
+const int TRIGGER_PIN = 3; // Trigger Pin on Sensor that receives the trigger impulse
 
 int distance_CM = 0;
 int distance_IN = 0;
@@ -20,9 +20,9 @@ void loop()
     delayMicroseconds(10); // Wait for 10 uS
     digitalWrite(TRIGGER_PIN, LOW); // Clear The Sensor
 
-    distance_CM = pulseIn(ECHO_PIN, HIGH) / 58;
-    distance_IN = pulseIn(ECHO_PIN, HIGH) / 148;
+    distance_CM = pulseIn(ECHO_PIN, HIGH) / 58; // Calculating distance in CMs
+    distance_IN = pulseIn(ECHO_PIN, HIGH) / 148; // Calculating distance in Inches
 
-    Serial.println(distance_CM);
-    delay(1000);
+    Serial.println(distance_CM); // Printing Distance (Example is in CMs)
+    delay(1000); // Wait for one second before sending the next wave
 }
