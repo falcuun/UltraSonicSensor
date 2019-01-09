08beba14c7e1de22fb718f118ca84b628686dc19
falcuun <falcuun@gmail.com>
2019-01-09 22:52:29 +0100

    Added basic logic for recieving time duration

---
+++ USSensor.ino
@@ -0,0 +1,25 @@
+
+const int ECHO_PIN = 5;
+const int TRIGGER_PIN = 3;
+
+int distance_CM = 0;
+int distance_IN = 0;
+
+void setup()
+{
+    pinMode(TRIGGER_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    digitalWrite(TRIGGER_PIN, LOW); // Clear The sensor
+    delayMicroseconds(5); // Wait for 5 uS
+    digitalWrite(TRIGGER_PIN, HIGH); // Send Impulse to the Trigger pin to sensor
+    delayMicroseconds(10); // Wait for 10 uS
+    digitalWrite(TRIGGER_PIN, LOW); // Clear The Sensor
+
+    Serial.println(pulseIn(ECHO_PIN, HIGH) / 58);
+    delay(1000);
+}
